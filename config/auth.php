b31cbd6462df585461f7f4411f7912b76e4ebb03
Muhammad Ruhiyat <muhammadruhiyat020998@gmail.com>
2023-10-04 18:28:43 +0700

    modif auth

--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,10 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+        'api' => [
+            'drive' => 'jwt',
+            'provide' => 'users',
+        ],
     ],
 
     /*
