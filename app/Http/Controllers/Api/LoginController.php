a7bbcc32036a62ec275916724ca1e5d355bcb878
Muhammad Ruhiyat <muhammadruhiyat020998@gmail.com>
2023-10-13 18:23:34 +0700

    add new login controller

---
+++ app/Http/Controllers/Api/LoginController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class LoginController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        $validator = Validator::make($request->all(), 
+        [
+            'email' => 'required',
+            'password' => 'required|min:8'
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $data = [
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ];
+        
+        // $credentials = $request->only('email', 'password');
+
+        if (!$token = auth()->guard('api')->attempt($data)) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Password Anda salah'
+            ], 401);
+        }else{
+            return response()->json([
+                'success' => true,
+                'user' => auth()->guard('api')->user(),
+                'token' => $token
+            ], 200);
+        }
+
+        
+    }
+}
